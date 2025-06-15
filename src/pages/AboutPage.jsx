0cb18d524ed36c55e31268e1bccc5d724015ed2a
Kaushal Sutar <kaushalsutar92@gmail.com>
2025-06-15 17:44:08 +0530

    first commit

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header/Header";
+const AboutPage = () => {
+    return (
+        <div>
+            <Header/>
+            <h1>This is About the page</h1>
+        </div>
+    );
+}
+
+export default AboutPage;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header/Header';
+const HomePage = () => {
+    return(
+         <div>
+                <Header/>                
+            <h1>Welcome to VCK OJT App</h1>
+            <p>This is the home page of the VCK OJT application.</p>
+         </div>
+    )
+}
+
+export default HomePage;
